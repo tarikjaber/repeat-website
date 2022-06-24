2fefb40a857d8730ad925cf87fffff1e86bd7cdb
Jaber <T.Jaber@medpace.medpace.com>
2022-06-24 10:18:53 -0400

    Add pause/play

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var repeatInterval = prompt("Enter repeat interval (minutes): ")
 
 var args = repeatInterval.split(" ");
 var repeatTimeSeconds;
+var isPaused = false;
 
 if (args.length == 2) {
     repeatTimeSeconds = parseInt(args[0]) * 60 + parseInt(args[1]);
@@ -14,16 +15,30 @@ console.log(repeatTimeSeconds);
 var secondsPassed = 0;
 
 setInterval(function () {
-    secondsPassed++;
-    if (secondsPassed == repeatTimeSeconds) {
-        console.log("Repeat!");
-        secondsPassed = 0;
+    if (!isPaused) {
+        secondsPassed++;
+        if (secondsPassed == repeatTimeSeconds) {
+            console.log("Repeat!");
+            secondsPassed = 0;
+        }
+        var secondsLeft = repeatTimeSeconds - secondsPassed;
+        var minutesLeft = Math.floor(secondsLeft / 60);
+        var secondsLeft = secondsLeft % 60;
+        var minutesString = String(minutesLeft).padStart(2, '0');
+        var secondsString = String(secondsLeft).padStart(2, '0');
+        document.title = `${minutesString}:${secondsString}`;
+        console.log(`${minutesString}:${secondsString}`);
     }
-    var secondsLeft = repeatTimeSeconds - secondsPassed;
-    var minutesLeft = Math.floor(secondsLeft / 60);
-    var secondsLeft = secondsLeft % 60;
-    var minutesString = String(minutesLeft).padStart(2, '0');
-    var secondsString = String(secondsLeft).padStart(2, '0');
-    document.title = `${minutesString}:${secondsString}`;
-    console.log(`${minutesString}:${secondsString}`);
 }, 1000);
+
+// Buttons
+var pauseButton = document.getElementById("pause");
+var playButton = document.getElementById("play");
+
+pauseButton.addEventListener('click', function (e) {
+    isPaused = true;
+});
+
+playButton.addEventListener('click', function (e) {
+    isPaused = false;
+});
